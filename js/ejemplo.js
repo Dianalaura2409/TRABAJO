56ed40e394f6a348b1d726b5017bbcb00f1c0267
Dianalaura <dianalaura2409@gmail.com>
2022-02-11 12:55:35 -0600

    se suben archivos

---
+++ js/ejemplo.js
@@ -0,0 +1,18 @@
+const boton = document.getElementById('botoncito');
+
+const input  = document.getElementById('cuadrito');
+const marco = document.getElementsByClassName('marco');
+console.log(marco);
+boton.setAttribute('value','Cambio texto');
+input.setAttribute('value','Que dormidos que estan');
+
+
+function clickButton (){
+const marco=document.getElementById("h");
+console.log("cambiamos de fondo");
+console.log(marco);
+marco.style.background="black";
+
+   }
+
+   

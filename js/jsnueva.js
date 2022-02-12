2fc6a04e455b7e8aa75148c27caf9c437b929d79
Dianalaura <dianalaura2409@gmail.com>
2022-02-12 01:00:44 -0600

    cambio de color en boton deshabilitar

---
+++ js/jsnueva.js
@@ -0,0 +1,23 @@
+const boton = document.getElementById('botoncito');
+const boton1 = document.getElementById('boton1');
+const boton3 = document.getElementById('boton3');
+const marco = document.getElementsByClassName('marco');
+const marco1 = document.getElementsByClassName('marco1');
+
+function clickButton (){
+const marco=document.getElementById("h");
+console.log(marco);
+marco.style.background="yellow";
+marco.style.color="black";
+document.getElementById("botoncito").disabled = true;
+   }
+
+
+function clickBoton2(){
+const marco1=document.getElementById("h1");
+console.log(marco1);
+marco1.style.background="white";
+marco1.style.color="blue";
+document.getElementById("h").disabled = true;
+}
+
